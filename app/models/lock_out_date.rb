c15b770bf2100d15168a48cc8038a32b4d1976d5
pvdvreede <paul@vdvreede.net>
2013-03-19 12:45:43 +1100

    Created lockout admin page to enabled and disable locks.

--- app/models/lock_out_date.rb
+++ app/models/lock_out_date.rb
@@ -1,9 +1,20 @@
 class LockOutDate < ActiveRecord::Base
   unloadable
 
-  def ==(other)
-    self.month == other.month &&
+  def <=>(other)
+    if self.month == other.month &&
       self.year == other.year
+      return 0
+    end
+
+    if self.year < other.year
+      return -1
+    end
+
+    if self.month < other.month
+      return -1
+    end
+
+    1
   end
-  alias :eql? :==
 end
--- init.rb
+++ init.rb
@@ -7,4 +7,8 @@ Redmine::Plugin.register :lock_out do
   version '0.9.0'
   url 'https://github.com/messagexchange/lock_out'
   author_url 'https://github.com/pvdvreede'
+
+  permission :alter_lock_dates, { :lock_out => :index, :lock_out => :lock, :lock_out => :unlock }
+
+  menu :top_menu, :lock_out, { :controller => 'lock_out', :action => 'index' }, :caption => "Lock out dates"
 end
